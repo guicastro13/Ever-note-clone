e210022b1d0db025124ce18da08008d3c1daab51
Guilherme Castro <guii_1@hotmail.com>
2022-07-28 18:37:25 -0300

    conexão com mongoDB

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI, {
-        
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Conectado com MONGODB!"))
+  .catch((error) => console.log(error));
